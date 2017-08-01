547aa6f534410dccabd69a29f390b12285881deb
KleinerHacker <Kleiner_Hacker@freenet.de>
2017-08-01 18:24:52 +0200

    update for tray icon

---
+++ JoyConToPC/JoyConToPC.TrayIcon/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
+
+namespace JoyConToPC.TrayIcon
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly NotifyIcon _notifyIcon;
+
+        public App()
+        {
+            _notifyIcon = new NotifyIcon
+            {
+                Icon = TrayIcon.Properties.Resources.CP050,
+                Text = "JoyCon Driver",
+                ContextMenu = new ContextMenu(new [] {
+                    new MenuItem("Exit", (sender, args) => Shutdown(0)), 
+                })
+            };
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _notifyIcon.Visible = true;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _notifyIcon.Visible = false;
+            base.OnExit(e);
+        }
+    }
+}
